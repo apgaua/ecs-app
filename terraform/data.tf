115c0178e243ac52585fbe214a9ff67c19d39d32
apgaua <apgaua.sousa@gmail.com>
2025-02-04 20:58:31 -0300

    Set app variables

---
+++ terraform/data.tf
@@ -0,0 +1,11 @@
+data "aws_ssm_parameter" "vpc_id" {
+  name = var.ssm_vpc_id
+}
+
+data "aws_ssm_parameter" "listener" {
+  name = var.ssm_listener
+}
+
+data "aws_ssm_parameter" "privatesubnets" {
+  name = var.ssm_private_subnets
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {}
+variable "service_name" {}
+variable "cluster_name" {}
+variable "vpc_id" {}
+variable "private_subnets" {}
+variable "service_port" {}
+variable "service_cpu" {}
+variable "service_memory" {}
+variable "ssm_vpc_id" {}
+variable "ssm_listener" {}
+variable "ssm_private_subnets" {}
